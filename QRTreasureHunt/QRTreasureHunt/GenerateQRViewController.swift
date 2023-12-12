9dbb4f1b84da8290f441d096486476791c9782c2
Lucas C Barros <Lucas_cb@icloud.com>
2023-12-11 20:11:40 -0800

    Generating QR Code from Strings

--- QRTreasureHunt/QRTreasureHunt/GenerateQRViewController.swift
+++ QRTreasureHunt/QRTreasureHunt/GenerateQRViewController.swift
@@ -3,28 +3,44 @@
 //  QRTreasureHunt
 //
 //  Created by Lucas C Barros on 2023-12-10.
-//
+//  Ref: https://www.youtube.com/watch?v=6ZATcKiol88
 
 import UIKit
 
 class GenerateQRViewController: UIViewController {
 
+//    var generateButton = UIButton()
+    var codeView = UIImageView()
+//    var codeStyleControl = UISegmentedControl()
+    
+    
+    var dataToCode = "Hello World!"
+    var filter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .green
-        // Do any additional setup after loading the view.
+        
+//        self.view.addSubview(generateButton)
+//        generateButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(codeView)
+        codeView.translatesAutoresizingMaskIntoConstraints = false
+        codeView
+            .centerToSuperview()
+            .heightTo(200)
+            .aspectRatio()
+            .backgroundColor = .darkGray
+        
+        let data = dataToCode.data(using: .ascii, allowLossyConversion: false)
+        
+        let transform = CGAffineTransform(scaleX: 10, y: 10)
+        filter = CIFilter(name: "CIQRCodeGenerator")
+        filter.setValue(data, forKey: "inputMessage")
+        
+        let image = UIImage(ciImage: filter.outputImage!.transformed(by: transform))
+        
+        codeView.image = image
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
